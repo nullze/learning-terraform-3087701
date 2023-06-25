fd7917cf421ab879f0604b12b589b7a55c740361
Jake A <57538995+nullze@users.noreply.github.com>
2023-06-24 22:55:42 -0600

    Fixing Errors in outputs.tf file

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "instance_ami" {
-  value = aws_instance.web.ami
+  value = aws_instance.blog.ami #Changing from "web" to "blog" to match main.tf file
 }
 
 output "instance_arn" {
-  value = aws_instance.web.arn
+  value = aws_instance.blog.arn # Changing from "web" to "blog" to match main.tf file
 }
